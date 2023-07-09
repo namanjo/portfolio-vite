e3c557e010ad33e221d6224cd6c69aa7839a504f
Naman Joshi <naman14joshi@gmail.com>
2023-07-09 20:45:22 +0530

    first commit: set up vite, tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Sidebar from './components/Sidebar'
+
+function App() {
+
+  return (
+      <div>
+        <Sidebar />
+      </div>
+  )
+}
+
+export default App
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {AiOutlineMenu} from 'react-icons/ai'
+
+
+
+const Sidebar = () => {
+  return (
+    <div>
+        <AiOutlineMenu />
+    </div>
+  )
+}
+
+export default Sidebar
