bf73a5db366e41166dc51e3bed4759c2672fbfaf
Naman Joshi <naman14joshi@gmail.com>
2023-07-10 15:34:48 +0530

    feat: added sidebar sestion

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,71 @@
-import React from 'react'
-import {AiOutlineMenu} from 'react-icons/ai'
-
+import React, {useState} from 'react'
+import {AiOutlineHome, AiOutlineMail, AiOutlineMenu, AiOutlineProject} from 'react-icons/ai'
+import {GrProjects} from 'react-icons/gr'
+import {BsPerson} from 'react-icons/bs'
 
 
 const Sidebar = () => {
+
+    const [nav, setNav] = useState(false);
+    
+    const handleNav = () => {
+        setNav(!nav);
+    };
+
   return (
     <div>
-        <AiOutlineMenu />
+        <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 md:hidden' />
+        {
+            nav ? (
+                <div className='w-full h-screen bg-white/90 flex flex-col justify-center items-center'>
+                    <a href='#main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <AiOutlineHome size={20} /> <span className='pl-4'>Home</span>
+                    </a>
+
+                    <a href='#work' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <GrProjects size={20} /> <span className='pl-4'>Work</span>
+                    </a>
+
+                    <a href='#projects' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <AiOutlineProject size={20} /> <span className='pl-4'>Projects</span>
+                    </a>
+
+                    <a href='#main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <BsPerson size={20} /> <span className='pl-4'>Resume</span>
+                    </a>
+
+                    <a href='#contact' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <AiOutlineMail size={20} /> <span className='pl-4'>Contact</span>
+                    </a>
+                </div>
+            ) : (
+                ''
+            )}
+
+            <div className='md:block hidden fixed top-[25%]'>
+                <div className='flex flex-col'>
+                    <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <AiOutlineHome size={20} />
+                    </a>
+
+                    <a href="#work" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <GrProjects size={20} />
+                    </a>
+
+                    <a href="#projects" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <AiOutlineProject size={20} />
+                    </a>
+
+                    <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <BsPerson size={20} />
+                    </a>
+
+                    <a href="#contact" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <AiOutlineMail size={20} />
+                    </a>
+                </div>
+            </div>
+
     </div>
   )
 }
